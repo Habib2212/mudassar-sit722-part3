7d59cdf100dd26edd6daa5aa359d9b6b323429b1
Mudassar Hibib <s223049696@deakin.edu.au>
2024-09-29 04:02:20 +1000

    completed the deployment

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://mudassar_sit722_part3_user:gr2F32IrESvncwp8Na7MOpIwqd0qz8fR@dpg-crs09n0gph6c738n26g0-a.oregon-postgres.render.com/mudassar_sit722_part3')
--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://mudassar_sit722_part3_user:gr2F32IrESvncwp8Na7MOpIwqd0qz8fR@dpg-crs09n0gph6c738n26g0-a.oregon-postgres.render.com/mudassar_sit722_part3" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
